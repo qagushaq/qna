0c0caf5581dfe9b69860ee9897d6c90cb4fe54de
qagushaq <qwerty24164444@gmail.com>
2021-05-25 14:47:15 +0300

    create tests for new,edit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,25 @@
 class QuestionsController < ApplicationController
+
+  before_action :load_question, only: %i[show edit update destroy]
+
   def index
     @questions = Question.all
   end
 
   def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def load_question
     @question = Question.find(params[:id])
   end
+
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create_list(:question, 3) }
+
   describe 'GET #index' do
   let(:questions) { create_list(:question, 3) }
 
@@ -16,7 +18,6 @@ RSpec.describe QuestionsController, type: :controller do
 end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
 
     before { get :show, params: { id: question } }
 
@@ -26,6 +27,31 @@ end
 
     it 'renders show view' do
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+
+    before { get :edit, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
     end
   end
 
