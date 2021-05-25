c29633b07d7006f1ce84aeaad0d31645f250d5e7
qagushaq <qwerty24164444@gmail.com>
2021-05-25 14:37:55 +0300

    create tests for controller_show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe QuestionsController, type: :controller do
   end
 end
 
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+
 end
