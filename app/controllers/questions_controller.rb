899bb58d75e5c3eb068a0c295bdc1672a0dd3f56
qagushaq <qwerty24164444@gmail.com>
2021-05-26 21:01:28 +0300

    tests for creating a question only by an authorized user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-
+  
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :load_question, only: %i[show edit update destroy]
 
   def index
@@ -20,7 +21,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
     end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 feature 'User can sign in', %q{
-In order to ask questions
-As an unauthenticated user
-I'd like to be able to sign in
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to sign in
 } do
 
-given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+  given(:user) { create(:user) }
 
-background { visit new_user_session_path }
+  background { visit new_user_session_path }
 
-scenario 'Registered user tries to sign in' do
-  fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
-  click_on 'Log in'
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
 
-  expect(page).to have_content 'Signed in successfully.'
-end
+    expect(page).to have_content 'Signed in successfully.'
+  end
 
-scenario 'Unregistered user tries to sign in' do
-  fill_in 'Email', with: 'wrong@test.com'
-  fill_in 'Password', with: '12345678'
-  click_on 'Log in'
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
 
-  expect(page).to have_content 'Invalid Email or password.'
-end
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 end
