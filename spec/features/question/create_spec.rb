d8906a8b5f6e0da5861cb827a3d590d487a51b6d
qagushaq <qwerty24164444@gmail.com>
2021-05-26 21:31:01 +0300

    fix question create test

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -35,8 +35,7 @@ feature 'User can create question', %q{
 
   scenario 'Unauthenticated user tries to ask a question' do
     visit questions_path
-    click_on 'Ask question'
 
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(page).to_not have_link 'Ask question'
   end
 end
