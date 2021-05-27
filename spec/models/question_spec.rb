4e3f5aa5dea70f09cbe537d03339606900b29ec9
qagushaq <qwerty24164444@gmail.com>
2021-05-27 12:13:39 +0300

    fix question_spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  it { should have_many :questions }
-  it { should have_many :answers }
+RSpec.describe Question, type: :model do
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should belong_to :user }
 
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 end
