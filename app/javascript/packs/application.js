51f7f2640502a20395a3d2c38f641a1b0fb8c986
qagushaq <qwerty24164444@gmail.com>
2021-06-01 00:13:08 +0300

    ajax edit questions

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require ("utilities/answer_edit_form")
+require ("utilities/question_edit_form")
 
 import "bootstrap"
 
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -135,27 +135,29 @@ RSpec.describe QuestionsController, type: :controller do
       before { login(user) }
 
       it 'assigns the requested question to @question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
+        patch :update, params: { id: question, question: attributes_for(:question) }, format: :js
         expect(assigns(:question)).to eq question
       end
 
       it 'changes question attributes' do
-        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
+        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }, format: :js
         question.reload
 
         expect(question.title).to eq 'new title'
         expect(question.body).to eq 'new body'
       end
 
-      it 'redirects to updated question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
-        expect(response).to redirect_to question
+      it 'render update view' do
+        patch :update, params: { id: question, question: attributes_for(:question) }, format: :js
+        expect(response).to render_template :update
       end
     end
 
     context 'with invalid attributes' do
-      before { login(user) }
-      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+      before do
+        login(user)
+        patch :update, params: { id: question, question: attributes_for(:question, :invalid) }, format: :js
+      end
 
       it 'does not change question' do
         question.reload
@@ -164,8 +166,8 @@ RSpec.describe QuestionsController, type: :controller do
         expect(question.body).to eq 'Question Text'
       end
 
-      it 're-renders edit view' do
-        expect(response).to render_template :edit
+      it 'renders update view' do
+        expect(response).to render_template :update
       end
     end
 
