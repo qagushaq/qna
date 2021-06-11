1bbef6f2dc5e0c31fff2cb2af45842478cb81559
qagushaq <qwerty24164444@gmail.com>
2021-06-11 20:59:16 +0300

    User can add multiple links to a question or answer

--- spec/features/answers/add_links_spec.rb
+++ spec/features/answers/add_links_spec.rb
@@ -8,9 +8,10 @@ feature 'User can add links to answer', %q{
 
   given(:user) {create(:user)}
   given!(:question) {create(:question)}
-  given(:gist_url) {'https://gist.github.com/qagushaq/9e8dfcadab09e7bb941bbf6ac0ea1b4d'}
+  given(:gist_url) { 'https://gist.github.com/qagushaq/9e8dfcadab09e7bb941bbf6ac0ea1b4d' }
+  given(:mail_url) { 'https://mail.ru' }
 
-  scenario 'User adds link when give an answer', js: true do
+  scenario 'User adds links when give an answer', js: true do
     sign_in(user)
 
     visit question_path(question)
@@ -20,10 +21,18 @@ feature 'User can add links to answer', %q{
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
+    click_on 'Add link'
+
+    within all('.new-link').last do
+      fill_in 'Link name', with: 'Mail'
+      fill_in 'Url', with: mail_url
+    end
+
     click_on 'Answer'
-    
+
     within '.answers' do
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Mail', href: mail_url
     end
   end
 
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -8,8 +8,9 @@ feature 'User can add links to question', %q{
 
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/qagushaq/9e8dfcadab09e7bb941bbf6ac0ea1b4d' }
+  given(:mail_url) { 'https://mail.ru' }
 
-  scenario 'User adds link when asks question' do
+  scenario 'User adds links when asks question', js: true do
     sign_in(user)
     visit new_question_path
 
@@ -19,9 +20,17 @@ feature 'User can add links to question', %q{
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
+    click_on 'Add link'
+
+    within all('.new-link').last do
+      fill_in 'Link name', with: 'Mail'
+      fill_in 'Url', with: mail_url
+    end
+
     click_on 'Ask'
 
     expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'Mail', href: mail_url
   end
 
 end
