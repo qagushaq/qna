496326e6de33d4486331e227d9815ed1053275ff
qagushaq <qwerty24164444@gmail.com>
2021-06-14 16:46:06 +0300

    ajah forms

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -18,7 +18,17 @@ class AnswersController < ApplicationController
   def create
     @answer = @question.answers.new(answer_params)
     @answer.user = current_user
-    @answer.save
+
+    respond_to do |format|
+      if @answer.save
+        format.html {render @answer}
+      else
+        format.html do
+          render partial: 'shared/errors', locals: { resource: @answer },
+                              status: :unprocessable_entity
+        end
+      end
+    end
   end
 
   def update
