5025cb0814fc5c9bde45ea5c388034448d69392c
qagushaq <qwerty24164444@gmail.com>
2021-06-22 19:24:46 +0300

    answer action_cable

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,6 +43,7 @@ class QuestionsController < ApplicationController
 
   def load_question
     @question = Question.with_attached_files.find(params[:id])
+    gon.question_id = @question.id
   end
 
   def question_params
