7f3dc7ba60b6d01cb8c640bc91f38bbfc3c1f451
qagushaq <qwerty24164444@gmail.com>
2021-06-26 23:09:53 +0300

    factories for user&auth

--- spec/factories/authorizations.rb
+++ spec/factories/authorizations.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory :authorization do
-    user { nil }
+    user
     provider { "MyString" }
-    uid { "MyString" }
+    uid { "123" }
+
+    trait :github do
+      provider { "github" }
+    end
+
+    trait :vkontakte do
+      provider { "vkontakte" }
+    end
   end
 end
