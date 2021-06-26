ec824827a7866f4e44e51752aef2fee68a14b4cd
qagushaq <qwerty24164444@gmail.com>
2021-06-26 23:12:01 +0300

    add helpers for auth tests

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -5,4 +5,12 @@ module FeatureHelpers
     fill_in 'Password', with: user.password
     click_on 'Log in'
   end
+
+  def auth_with(provider, email = nil)
+    hash = OmniAuth::AuthHash.new({ provider: provider.to_s, uid: '123',
+                                    info: { name: 'test test' }
+    })
+    hash.info.merge!(email: email) if email
+    OmniAuth.config.mock_auth[provider] = hash
+  end
 end
