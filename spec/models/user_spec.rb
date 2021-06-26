f062fcb4be1ed7b413156c0d85a453df442cd6a4
qagushaq <qwerty24164444@gmail.com>
2021-06-26 22:20:34 +0300

    fix spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,10 +25,10 @@ RSpec.describe User, type: :model do
   describe '.find_for_oauth' do
     let!(:user) { create(:user) }
     let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
-    let(:service) { double('Services::FindForOauth') }
+    let(:service) { double('FindForOauth') }
 
     it 'calls Services::FindForOauth' do
-      expect(Services::FindForOauth).to receive(:new).with(auth).and_return(service)
+      expect(FindForOauth).to receive(:new).with(auth).and_return(service)
       expect(service).to receive(:call)
       User.find_for_oauth(auth)
     end
